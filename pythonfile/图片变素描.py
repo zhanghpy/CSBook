0e238112dc7a2bee95cfad520939c76040302230
zhanghao <zhanghao@yuanshanbao.com>
2024-05-21 18:52:22 +0800

    commit pythonfile file

---
+++ pythonfile/图片变素描.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 18:23:07 2024
+
+@author: z1513
+"""
+
+import cv2
+
+path = r"C:\Users\z1513\Desktop\1.jpg"
+image = cv2.imread(path)
+cv2.imshow("Dog", image)
+cv2.waitKey(0)
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("New Dog", gray_image)
+cv2.waitKey(0)
+
+inverted_image = 255 - gray_image
+cv2.imshow("Inverted", inverted_image)
+cv2.waitKey()
+
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+
+inverted_blurred = 255 - blurred
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+cv2.imshow("Sketch", pencil_sketch)
+cv2.waitKey(0)
+
+cv2.imshow("original image", image)
+cv2.imshow("pencil sketch", pencil_sketch)
+cv2.waitKey(0)
